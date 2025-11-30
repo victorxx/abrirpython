68cf17c5a595696a7fca8c0f79651885e84b25cf
victorxx <victorsaib@gmail.com>
2025-11-30 14:14:21 -0300

    Create unicodetabela.py

---
+++ unicodetabela.py
@@ -0,0 +1,21 @@
+Código	Significado	Exemplo
+Lu	Letter, Uppercase (letra maiúscula)	A, B, C
+Ll	Letter, Lowercase (letra minúscula)	a, b, c
+Lt	Letter, Titlecase (letra título, como ǅ)	ǅ
+Lm	Letter, Modifier (letras modificadoras)	ʰ, ˡ
+Lo	Letter, Other (outras letras)	א, λ
+Mn	Mark, Nonspacing (acentos que não ocupam espaço)	́, ̃
+Mc	Mark, Spacing Combining (acentos que ocupam espaço)	᾿, ῾
+Nd	Number, Decimal Digit (números decimais)	0, 1, 2
+Nl	Number, Letter (números representados como letra)	Ⅰ, Ⅴ
+No	Number, Other (outros números)	², ³
+Pc	Punctuation, Connector (sublinhado _)	_
+Pd	Punctuation, Dash (traços)	-, –
+Ps	Punctuation, Open (abre parênteses, colchetes)	(, [
+Pe	Punctuation, Close (fecha parênteses, colchetes)	), ]
+Po	Punctuation, Other (outros sinais de pontuação)	., !, ?
+Zs	Separator, Space (espaço normal)	' '
+Zl	Separator, Line (quebra de linha)	\u2028
+Zp	Separator, Paragraph (separador de parágrafo)	\u2029
+Cc	Other, Control (caracteres de controle)	\n, \t
+Cf	Other, Format (marcadores de formatação)	\u200B (zero-width space)
