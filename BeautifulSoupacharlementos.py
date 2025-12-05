48fa22802b7b846bcdbbe831d5884927a4360c43
victorxx <victorsaib@gmail.com>
2025-12-05 19:21:17 -0300

    Update BeautifulSoupacharlementos.py

--- BeautifulSoupacharlementos.py
+++ BeautifulSoupacharlementos.py
@@ -1,14 +1,23 @@
-import os
 from bs4 import BeautifulSoup
-import time
+import os
 
-desktop=os.path.join(os.path.expanduser('~'),'Desktop')
-nome='pagina.html'
-juntar=os.path.join(desktop,nome)
-with open(juntar,'r') as file:
-    lendo=file.read()
+# Caminho para a área de trabalho
+desktop = os.path.join(os.path.expanduser('~'), 'Desktop')
+nome_arquivo = 'pagina.html'
+caminho = os.path.join(desktop, nome_arquivo)
 
-soup=BeautifulSoup(lendo,'html.parser')
-soup.find_all('a')
-quantidade=len(soup)
-print(quantidade)
+# Abrindo o arquivo HTML
+with open(caminho, 'r', encoding='utf-8') as file:
+    conteudo = file.read()
+
+# Criando o objeto BeautifulSoup
+soup = BeautifulSoup(conteudo, 'html.parser')
+
+# Encontrando todos os botões
+botoes = soup.find_all('button')
+
+# Contando os botões
+quantidade = len(botoes)
+
+print(f"Quantidade de botões encontrados: {quantidade}")
+print('Finalizando...')
