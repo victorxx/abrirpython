43ad34d136296290743e2d6bc85c54e98d331ae9
victorxx <victorsaib@gmail.com>
2025-12-05 11:50:35 -0300

    Create modificarvalor.py

---
+++ modificarvalor.py
@@ -0,0 +1,20 @@
+import os
+from bs4 import BeautifulSoup
+
+
+
+desktop=os.path.join(os.path.expanduser('~'),'Desktop')
+arquivo='pagina.html'
+juntar=os.path.join(desktop,arquivo)
+if os.path.exists(juntar):
+    print('ok podemos continuar')
+    with open(juntar,'r') as file:
+        lendo=file.read()
+    soup=BeautifulSoup(lendo,'html.parser')
+    frutas=soup.find_all('li')
+    primeiro=frutas[1].string='maca'
+    
+    print('finalizado...')
+else:
+
+    print('ok deu algo errado...')
