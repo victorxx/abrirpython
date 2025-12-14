6d3893336139fcf5b05c6730f8eb3cc151e23dd6
victorxx <victorsaib@gmail.com>
2025-12-14 11:28:17 -0300

    Create enumratepython.py

---
+++ enumratepython.py
@@ -0,0 +1,8 @@
+Aluno=[
+    ['Carlos',5.0],
+    ['Karla',3.0],
+    ['Bola',5.9]
+]
+for i , (notas,nome) in enumerate(Aluno):
+    print(f'ok foi sim Notas{notas} e agora nomes {nome}')
+    print('ok foi ')
