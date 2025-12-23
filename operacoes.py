acdd1bc285da8ca655f7e71ddcb4a31d0cb63c0c
victorxx <victorsaib@gmail.com>
2025-12-22 22:13:08 -0300

    Create operacoes.py

---
+++ operacoes.py
@@ -0,0 +1,15 @@
+
+a=float(input('Digite o primeiro termo'))
+b=float(input('Digite o segundo termo'))
+op=input('escolha a operação(+-,*,/)')
+
+if op=='+':
+    print(a+b)
+elif op=='-':
+    print(a-b)
+elif op=='*':
+    print(a*b)
+elif op=='/':
+    print(a-b)
+else:
+    print('operação inválida')
